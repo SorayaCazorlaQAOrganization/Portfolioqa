69ed917310ad82f70d2d5927cbd201c3c6b2d3a0
Soraya Cazorla Guillén <sorayacazorlag@gmail.com>
2026-03-27 19:26:11 +0100

    Update update.sql

---
+++ sql/update.sql
@@ -0,0 +1,9 @@
+--Change the email of ID 33 to 'lauratest@gmail.com'--
+UPDATE form
+SET email = 'lauratest@gmail.com'
+WHERE id = 33;
+
+--Change age 20 to 21--
+UPDATE form
+SET age = 21
+WHERE age = 20;
